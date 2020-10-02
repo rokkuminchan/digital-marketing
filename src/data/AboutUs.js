6d78adbda87d16f0d03cf6b6bfd33b3579141a40
tuanpham95 <nevergiveup1195@gmail.com>
2020-10-02 15:11:33 +0900

    Update AboutUs.js
    update WorldRecruitHistory,VietnamUniversity,VietnamAndJapan,companyInformation

--- src/data/AboutUs.js
+++ src/data/AboutUs.js
@@ -1,119 +1,231 @@
 const AboutUsData = {
-  title: "私たちは第二世代のベトナム人ITエンジニアのチームです。",
-  descripsion:
-    "高度IT人材として日本のDXに貢献するために、普通の日本人と同じように、日本語の読み書きや会話ができ、日本の文化を理解し、尊重し、日本人の働き方を学んでいます。また、最新のIT技術を学び、仕事に役立つシステムを設計・実装します。",
-  image: {
-    src: "",
-    alt: "",
-  },
-  mission: {
-    title: "",
-    descripsion: "",
+    title: "私たちは第二世代のベトナム人ITエンジニアのチームです。",
+    descripsion:
+        "高度IT人材として日本のDXに貢献するために、普通の日本人と同じように、日本語の読み書きや会話ができ、日本の文化を理解し、尊重し、日本人の働き方を学んでいます。また、最新のIT技術を学び、仕事に役立つシステムを設計・実装します。",
     image: {
-      src: "",
-      alt: "",
+        src: "",
+        alt: "",
     },
-  },
-  vision: {
-    title: "",
-    descripsion: "",
-    image: {
-      src: "",
-      alt: "",
+    mission: {
+        title: "",
+        descripsion: "",
+        image: {
+            src: "",
+            alt: "",
+        },
     },
-    items: [
-      {
-        title: "言葉の壁",
-        descripsion:
-          "普通の日本人と同じように、日本語の読み書きや会話ができる。",
-      },
-      {
-        title: "文化の壁",
-        descripsion: "日本の文化を理解し、尊重する。、保存する。",
-      },
-      {
-        title: "業務の壁",
-        descripsion: "日本人の働き方を学び、仕事に役立つシステムを設計する。",
-      },
-    ],
-  },
-  TeamCharacteristic: {
-    title: "",
-    descripsion: "",
-    image: {
-      src: "",
-      alt: "",
+    vision: {
+        title: "",
+        descripsion: "",
+        image: {
+            src: "",
+            alt: "",
+        },
+        items: [
+            {
+                title: "言葉の壁",
+                descripsion:
+                    "普通の日本人と同じように、日本語の読み書きや会話ができる。",
+            },
+            {
+                title: "文化の壁",
+                descripsion: "日本の文化を理解し、尊重する。、保存する。",
+            },
+            {
+                title: "業務の壁",
+                descripsion: "日本人の働き方を学び、仕事に役立つシステムを設計する。",
+            },
+        ],
     },
-    items: [
-      {
-        image: {
-          src: "",
-          alt: "",
-        },
+    TeamCharacteristic: {
+        title: "",
         descripsion: "",
-      },
-      {
         image: {
-          src: "",
-          alt: "",
+            src: "",
+            alt: "",
         },
+        items: [
+            {
+                image: {
+                    src: "",
+                    alt: "",
+                },
+                descripsion: "",
+            },
+            {
+                image: {
+                    src: "",
+                    alt: "",
+                },
+                descripsion: "",
+            },
+            {
+                image: {
+                    src: "",
+                    alt: "",
+                },
+                descripsion: "",
+            },
+            {
+                image: {
+                    src: "",
+                    alt: "",
+                },
+                descripsion: "",
+            },
+        ],
+    },
+    MemberCharacteristic: {
+        title: "",
         descripsion: "",
-      },
-      {
-        image: {
-          src: "",
-          alt: "",
-        },
-        descripsion: "",
-      },
-      {
-        image: {
-          src: "",
-          alt: "",
-        },
-        descripsion: "",
-      },
-    ],
-  },
-  MemberCharacteristic: {
-    title: "",
-    descripsion: "",
 
-    items: [
-      {
+        items: [
+            {
+                title: "",
+                image: {
+                    src: "",
+                    alt: "",
+                },
+                descripsion: "",
+            },
+            {
+                title: "",
+                image: {
+                    src: "",
+                    alt: "",
+                },
+                descripsion: "",
+            },
+            {
+                title: "",
+                image: {
+                    src: "",
+                    alt: "",
+                },
+                descripsion: "",
+            },
+            {
+                title: "",
+                image: {
+                    src: "",
+                    alt: "",
+                },
+                descripsion: "",
+            },
+        ],
+    },
+    MemberIntroduction: {
         title: "",
-        image: {
-          src: "",
-          alt: "",
-        },
         descripsion: "",
-      },
-      {
+        items: [
+            {
+                title: "",
+                image: {
+                    src: "",
+                    alt: "",
+                },
+                descripsion: "",
+            },
+            {
+                title: "",
+                image: {
+                    src: "",
+                    alt: "",
+                },
+                descripsion: "",
+            },
+            {
+                title: "",
+                image: {
+                    src: "",
+                    alt: "",
+                },
+                descripsion: "",
+            },
+            {
+                title: "",
+                image: {
+                    src: "",
+                    alt: "",
+                },
+                descripsion: "",
+            },
+            {
+                title: "",
+                image: {
+                    src: "",
+                    alt: "",
+                },
+                descripsion: "",
+            },
+            {
+                title: "",
+                image: {
+                    src: "",
+                    alt: "",
+                },
+                descripsion: "",
+            },
+
+        ],
+
+    },
+    WorldRecruitHistory: {
         title: "",
-        image: {
-          src: "",
-          alt: "",
-        },
         descripsion: "",
-      },
-      {
+        image: {
+            src: "",
+            alt: "",
+        },
+    },
+    VietnamUniversity: {
         title: "",
-        image: {
-          src: "",
-          alt: "",
-        },
         descripsion: "",
-      },
-      {
+
+        items: [
+            {
+                title: "",
+                image: {
+                    src: "",
+                    alt: "",
+                },
+                descripsion: "",
+                link: "",
+            },
+            {
+                title: "",
+                image: {
+                    src: "",
+                    alt: "",
+                },
+                descripsion: "",
+                link: "",
+            },
+            {
+                title: "",
+                image: {
+                    src: "",
+                    alt: "",
+                },
+                descripsion: "",
+                link: "",
+            },
+        ]
+
+    },
+    VietnamAndJapan: {
         title: "",
-        image: {
-          src: "",
-          alt: "",
-        },
         descripsion: "",
-      },
-    ],
-  },
+        image: {
+            src: "",
+            alt: "",
+        },
+    },
+    companyInformation: {
+        address: "",
+        phone: "",
+        fax: "",
+    }
 };
 
 export default AboutUsData;
