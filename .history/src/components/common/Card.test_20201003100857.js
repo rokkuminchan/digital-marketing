b88d7912a7b79a1fe6c4da03262cc84a40faa693
vuduchuy94 <vuduchuybk@gmail.com>
2020-10-03 10:23:31 +0900

    add card component

---
+++ .history/src/components/common/Card.test_20201003100857.js
@@ -0,0 +1,38 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import { act } from "react-dom/test-utils";
+import Card from "./Card";
+import test from "../../img/test.png";
+
+let container;
+
+const Data = {
+  img: {
+    src: test,
+    alt: "test",
+  },
+  title: "test title",
+  description: "test description",
+};
+
+beforeEach(() => {
+  container = document.createElement("div");
+  document.body.appendChild(container);
+});
+
+afterEach(() => {
+  document.body.removeChild(container);
+  container = null;
+});
+
+it("can render a component from data", () => {
+  ReactDOM.render(<Card data={Data} />, container);
+  const title = container.querySelector("h3");
+  const description = container.querySelector("p");
+  const img = container.querySelector("img");
+
+  expect(logo.src).toMatch(new RegExp(Data.img.src));
+  expect(logo.alt).toContain(logo.alt);
+  expect(title.textContent).toBe(Data.title);
+  expect(description.textContent).toBe(Data.description);
+});
---
+++ .history/src/components/common/Card_20201003095224.js
@@ -0,0 +1,12 @@
+import React from  "react"
+import "./Card.css"
+
+export default function Card({data}){
+    return
+    <area>
+    <img src= {data.img.src} alt = {data.img.alt} > </img>
+    <h3 className="card__title">{data.title}</h3>
+    <p className="card__description">{data.}</p>
+
+    </area>;
+}
