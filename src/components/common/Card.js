43053d8d5d070f99a6fe974d6901ce00dff581a3
Vien-world <vien@world-works.co.jp>
2020-10-05 16:31:12 +0900

    modifled Card.js img -> image

--- src/components/common/Card.js
+++ src/components/common/Card.js
@@ -4,7 +4,7 @@ import "./Card.css";
 export default function Card({ data }) {
   return (
     <div className="card">
-      <img className="card__img" src={data.img.src} alt={data.img.alt}></img>
+      <img className="card__img" src={data.image.src} alt={data.image.alt}></img>
       <h3 className="card__title">{data.title}</h3>
       <p className="card__description">{data.description}</p>
     </div>
