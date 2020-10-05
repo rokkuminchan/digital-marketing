8f3f46fa98a5f13bfdff1ba8134125fa8199c048
nevergiveup1195 <nevergiveup1195@gmail.com>
2020-10-05 09:13:49 +0900

    Update Mission.js

---
+++ src/components/sections/Mission.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./Mission.css";
+
+export default function Mission({ data }) {
+  return (
+    <section className="mission">
+      <h3 className="mission__title">{data.title}</h3>
+      <p className="mission__description">{data.description}</p>
+      <img className="mission__img" src={data.img.src} alt={data.img.alt}></img>
+    </section>
+  );
+}
