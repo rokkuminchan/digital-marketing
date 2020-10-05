205d53a43bc544d7ad3dc31461cbc5c9142024ec
imfine0104 <31641716+imfine0104@users.noreply.github.com>
2020-10-05 16:00:49 +0900

    MemberCharacteristic

--- src/pages/AboutUs.js
+++ src/pages/AboutUs.js
@@ -1,12 +1,13 @@
 import React from "react";
 import Layout from "../components/layout";
-import TeamCharacteristic from "../components/sections/aboutUs/MemberCharacteristic";
+import MemberCharacteristic from "../components/sections/aboutUs/MemberCharacteristic";
 import AboutUsData from "../data/AboutUs";
 
-export default function AboutUs({data}) {
-
-  var { teamCharacteristic } = AboutUsData;
-  return <Layout>
-    <TeamCharacteristic data={teamCharacteristic} />
-  </Layout>;
+export default function AboutUs({ data }) {
+  var { memberCharacteristic } = AboutUsData;
+  return (
+    <Layout>
+      <MemberCharacteristic data={memberCharacteristic} />
+    </Layout>
+  );
 }
