5376bdbb7a449d9fd492060edcd4751b027d9407
HaiYen1995 <yen@world-works.co.jp>
2020-10-05 16:15:49 +0900

    Relationship
    add hien thi

--- src/components/sections/aboutUs/Relationship.js
+++ src/components/sections/aboutUs/Relationship.js
@@ -1,15 +1,16 @@
 import React from "react";
 import "./Relationship.css";
+import Title from "../../common/Title";
 
 export default function Relationship({ data }) {
   return (
     <div className="relationship">
-      <h3 className="relationship__title">{data.title}</h3>
+      <Title data={data.title} />
       <p className="relationship__description">{data.description}</p>
       <img
         className="relationship__img"
-        src={data.img.src}
-        alt={data.img.alt}
+        src={data.image.src}
+        alt={data.image.alt}
       ></img>
     </div>
   );
--- src/pages/AboutUs.js
+++ src/pages/AboutUs.js
@@ -1,13 +1,16 @@
 import React from "react";
 import Layout from "../components/layout";
 import MemberCharacteristic from "../components/sections/aboutUs/MemberCharacteristic";
+import TeamCharacteristic from "../components/sections/aboutUs/MemberCharacteristic";
+import Relationship from "../components/sections/aboutUs/Relationship";
 import AboutUsData from "../data/AboutUs";
 
 export default function AboutUs({ data }) {
-  var { memberCharacteristic } = AboutUsData;
+  var { memberCharacteristic, relationship } = AboutUsData;
   return (
     <Layout>
       <MemberCharacteristic data={memberCharacteristic} />
+      <Relationship data={relationship} />
     </Layout>
   );
 }
