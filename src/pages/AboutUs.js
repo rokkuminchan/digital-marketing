4c936777c145c8af60b440d1389ed74392dc82a9
Guava <linh.guava94@gmail.com>
2020-10-05 17:54:00 +0900

    WorldRecruitHistoryを表示

--- src/pages/AboutUs.js
+++ src/pages/AboutUs.js
@@ -1,12 +1,8 @@
 import React from "react";
 import Layout from "../components/layout";
 import MemberCharacteristic from "../components/sections/aboutUs/MemberCharacteristic";
-<<<<<<< HEAD
 import WorldRecruitHistory from "../components/sections/aboutUs/WorldRecruitHistory";
 import TeamCharacteristic from "../components/sections/aboutUs/MemberCharacteristic";
-=======
-//import TeamCharacteristic from "../components/sections/aboutUs/MemberCharacteristic";
->>>>>>> 730c3dca57ef5b0ddf5f6f40eca76b2831a9902e
 import Relationship from "../components/sections/aboutUs/Relationship";
 import AboutUsData from "../data/AboutUs";
 
