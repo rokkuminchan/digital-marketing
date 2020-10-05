2a1fd9163d43e16bbb6e286cd0e0fee82d519de1
minhtienvo1991 <71481846+minhtienvo1991@users.noreply.github.com>
2020-10-05 17:55:40 +0900

    Update Card.js

--- src/components/common/Card.js
+++ src/components/common/Card.js
@@ -5,6 +5,7 @@ export default function Card({ data }) {
   return (
     <div className="card">
       <img className="card__img" src={data.img.src} alt={data.img.alt}></img>
+      <img className="card__img" src={data.image.src} alt={data.image.alt}></img>
       <h3 className="card__title">{data.title}</h3>
       <p className="card__description">{data.description}</p>
     </div>
