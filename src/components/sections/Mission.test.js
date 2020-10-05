b9dbb9f99728c1e0856fb6cf83a1e2343bde7dfe
nevergiveup1195 <nevergiveup1195@gmail.com>
2020-10-05 09:13:55 +0900

    Create Mission.test.js

---
+++ src/components/sections/Mission.test.js
@@ -0,0 +1,36 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import Mission from "./Mission";
+
+let container;
+
+const Data = {
+  title: "ミッション",
+  description: "高度IT人材で日本のDXに貢献する。",
+  img: {
+    src: "",
+    alt: "",
+  },
+};
+
+beforeEach(() => {
+  container = document.createElement("div");
+  document.body.appendChild(container);
+});
+
+afterEach(() => {
+  document.body.removeChild(container);
+  container = null;
+});
+
+it("can render a component from data", () => {
+  ReactDOM.render(<Mission data={Data} />, container);
+  const title = container.querySelector("h3");
+  const description = container.querySelector("p");
+  const img = container.querySelector("img");
+
+  expect(img.src).toMatch(new RegExp(Data.img.src));
+  expect(img.alt).toContain(Data.img.alt);
+  expect(title.textContent).toBe(Data.title);
+  expect(description.textContent).toBe(Data.description);
+});
