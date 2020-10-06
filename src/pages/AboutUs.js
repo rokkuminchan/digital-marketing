7c8590abd7e3dacce15c467bad6e9e2a6926b72b
VuongTran0104 <vuong@world-works.co.jp>
2020-10-06 11:27:41 +0900

    AboutUS.js

--- src/pages/AboutUs.js
+++ src/pages/AboutUs.js
@@ -3,11 +3,11 @@ import Layout from "../components/layout";
 import Mission from "../components/sections/aboutUs/Mission";
 import Vision from "../components/sections/aboutUs/Vision";
 import MemberCharacteristic from "../components/sections/aboutUs/MemberCharacteristic";
-import TeamCharacteristic from "../components/sections/aboutUs/MemberCharacteristic";
+//import TeamCharacteristic from "../components/sections/aboutUs/MemberCharacteristic";
 import MemberIntro from "../components/sections/aboutUs/MemberIntro";
 import WorldRecruitHistory from "../components/sections/aboutUs/WorldRecruitHistory";
 import Relationship from "../components/sections/aboutUs/Relationship";
-import VNUniversity from '../components/sections/aboutUs/VNUniversity';
+import VNUniversity from "../components/sections/aboutUs/VNUniversity";
 import AboutUsData from "../data/AboutUs";
 
 export default function AboutUs({ data }) {
@@ -16,7 +16,7 @@ export default function AboutUs({ data }) {
       <Mission data={AboutUsData.mission} />
       <Vision data={AboutUsData.vision} />
       <MemberCharacteristic data={AboutUsData.memberCharacteristic} />
-      <TeamCharacteristic data={AboutUsData.teamCharacteristic} />
+      {/* <TeamCharacteristic data={AboutUsData.teamCharacteristic} /> */}
       <MemberIntro data={AboutUsData.memberIntroduction} />
       <WorldRecruitHistory data={AboutUsData.worldRecruitHistory} />
       <Relationship data={AboutUsData.relationship} />
