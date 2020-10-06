4f6b0e76bb5602c052a887f8fa24abd76756cfca
minhtienvo1991 <71481846+minhtienvo1991@users.noreply.github.com>
2020-10-06 09:25:39 +0900

    Update AboutUs.js

--- src/pages/AboutUs.js
+++ src/pages/AboutUs.js
@@ -8,22 +8,12 @@ import Relationship from "../components/sections/aboutUs/Relationship";
 import AboutUsData from "../data/AboutUs";
 import Vision from "../components/sections/aboutUs/Vision"
 export default function AboutUs({ data }) {
-<<<<<<< HEAD
   var { memberCharacteristic, vision } = AboutUsData;
-=======
-  var { mission, memberCharacteristic, relationship } = AboutUsData;
-  var { worldRecruitHistory } = AboutUsData;
->>>>>>> f4cfc320d6241b5d4041fd9b75a11676f2de836e
   return (
     <Layout>
-      <Mission data={mission} />
+      {/* <Mission data={mission} /> */}
       <MemberCharacteristic data={memberCharacteristic} />
-<<<<<<< HEAD
       <Vision data={vision}/>
-=======
-      <WorldRecruitHistory data={worldRecruitHistory} />
-      <Relationship data={relationship} />
->>>>>>> f4cfc320d6241b5d4041fd9b75a11676f2de836e
     </Layout>
   );
 }
