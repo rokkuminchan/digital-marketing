60df95970faa55b5e073e6e36c0f2fcb85d76196
tuanpham95 <nevergiveup1195@gmail.com>
2020-10-06 10:22:22 +0900

    Update Mission.js
    ミッション中央

--- src/components/sections/aboutUs/Mission.js
+++ src/components/sections/aboutUs/Mission.js
@@ -1,12 +1,18 @@
 import React from "react";
 import "./Mission.css";
+import Title from "../../common/Title";
 
 export default function Mission({ data }) {
   return (
     <section className="mission">
-      <h3 className="mission__title">{data.title}</h3>
+      <Title data={data.title} />
+      {/* <h3 className="mission__title">{data.title}</h3> */}
       <p className="mission__description">{data.description}</p>
-      <img className="mission__img" src={data.image.src} alt={data.image.alt}></img>
+      <img
+        className="mission__img"
+        src={data.image.src}
+        alt={data.image.alt}
+      ></img>
     </section>
   );
 }
