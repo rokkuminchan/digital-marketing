1609544fd1541d35f904dc9f9f56ccb8b9f74aff
tuanpham95 <nevergiveup1195@gmail.com>
2020-10-06 10:22:30 +0900

    Update AboutUs.js

--- src/pages/AboutUs.js
+++ src/pages/AboutUs.js
@@ -12,12 +12,12 @@ export default function AboutUs({ data }) {
   return (
     <Layout>
       <Mission data={AboutUsData.mission} />
-      <Vision data ={AboutUsData.vision} />
+      <Vision data={AboutUsData.vision} />
       <MemberCharacteristic data={AboutUsData.memberCharacteristic} />
-      <TeamCharacteristic data ={AboutUsData.teamCharacteristic} />
-      <MemberIntro data ={AboutUsData.memberIntroduction} />
-      <WorldRecruitHistory data ={AboutUsData.worldRecruitHistory} />
-      <Relationship data ={AboutUsData.relationship} />
+      <TeamCharacteristic data={AboutUsData.teamCharacteristic} />
+      <MemberIntro data={AboutUsData.memberIntroduction} />
+      <WorldRecruitHistory data={AboutUsData.worldRecruitHistory} />
+      <Relationship data={AboutUsData.relationship} />
     </Layout>
   );
 }
