b3be74edf611ab053a424b5fb2411fe6c930f1e1
Vu Sinh <sinh@world-works.co.jp>
2020-10-06 09:04:18 +0900

    edit card component

--- src/components/common/Card.js
+++ src/components/common/Card.js
@@ -2,16 +2,14 @@ import React from "react";
 import "./Card.css";
 
 export default function Card({ data }) {
-	const { image, title, description } = data;
-	const { src, alt } = image;
 	return (
 		<div className="card">
 			<div className="card__img">
-				<img src={src} alt={alt}></img>
+				<img src={data.image.src} alt={data.image.alt}></img>
 			</div>
 			<div className="card__content">
-				<h3>{title}</h3>
-				<p>{description}</p>
+				<h3>{data.title}</h3>
+				<p>{data.description}</p>
 			</div>
 		</div>
 	);
