be4d35c4a58e23b37aae454bb58aec7b21914e2c
thainguyen1994 <71751911+thainguyen0109@users.noreply.github.com>
2020-10-07 09:19:42 +0900

    Footer-css-fix

--- src/components/sections/Footer.js
+++ src/components/sections/Footer.js
@@ -6,14 +6,23 @@ export default function Footer({ data }) {
   return (
     <footer className="footer">
       <Title data="WORLD WORKS" />
-      <a herf="Info@https://world-works.co.jp" className="footer__link">
-        Info@https://world-works.co.jp
-      </a>
-      <p className="footer__description">
-        住所: 〒500-8856 岐阜県岐阜市橋本町2-8 濃飛ニッセイビル6階
-      </p>
-      <p className="footer__description">TEL: 058-255-3066</p>
-      <p className="footer__description">FAX: 058-255-3633</p>
+      <div>
+        <a herf="Info@https://world-works.co.jp" className="footer__link">
+          Info@https://world-works.co.jp
+        </a>
+      </div>
+
+      <div className="footer__address">
+        <p>住所:</p>
+        <p>電話番号:</p>
+        <p>ＦＡＸ:</p>
+      </div>
+      <div className="footer__address-a">
+        <p>〒500-8856 岐阜県岐阜市橋本町2-8 濃飛ニッセイビル6階</p>
+        <p>058-255-3066</p>
+        <p>058-255-3633</p>
+      </div>
+
       <div className="footer__items">
         <a herf="Twitter" className="footer__items">
           Twitter
@@ -25,6 +34,7 @@ export default function Footer({ data }) {
           Instagram
         </a>
       </div>
+
       <p className="footer__description">
         Copyright
         (C)　愛知・岐阜・三重の人材派遣や業務請負など総合人材サービスはワールドグループ
