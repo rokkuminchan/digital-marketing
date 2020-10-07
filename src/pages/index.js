ddb32b17238e8892fef2f79165104316857b5b17
rokku <loc@world-works.co.jp>
2020-10-07 14:06:36 +0900

    add comment to gatsby branch just for testing if I can push to gatsby branch

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,6 +3,7 @@ import { Link } from "gatsby";
 import Layout from "../components/layout";
 import SEO from "../components/seo";
 
+//Home page
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
