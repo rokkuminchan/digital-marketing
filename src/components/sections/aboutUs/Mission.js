77478042cf6bfd3c753824aa15617ddd54e046fe
tuanpham95 <nevergiveup1195@gmail.com>
2020-10-07 09:42:44 +0900

    Update Mission.js

--- src/components/sections/aboutUs/Mission.js
+++ src/components/sections/aboutUs/Mission.js
@@ -5,12 +5,12 @@ import Title from "../../common/Title";
 export default function Mission({ data }) {
   return (
     <section className="mission">
-      <div className="combine">
+      <div className="mission__block1">
         <Title data={data.title} />
         {/* <h3 className="mission__title">{data.title}</h3> */}
         <p className="mission__description">{data.description}</p>
       </div>
-      <div className="combineImg">
+      <div className="mission__block2">
         <img
           className="mission__img"
           src={data.image.src}
