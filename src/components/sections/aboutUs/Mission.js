3616310ec54e6296937b9739eb3123ab3f6eac86
Vien-world <vien@world-works.co.jp>
2020-10-07 23:59:16 +0900

    modified style Mission

--- src/components/sections/aboutUs/Mission.js
+++ src/components/sections/aboutUs/Mission.js
@@ -7,21 +7,13 @@ import Image from "../../common/Image";
 export default function Mission({ data }) {
   return (
     <section className="mission">
-            
       <div className="mission__block1">
-                
-        <Title className="mission__title" data={data.title} />
-                
-        <Description className="mission__description" data={data.description} />
-              
+        <Title className="mission__title" data={data.title} />         
+        <Description className="mission__description" data={data.description} />  
       </div>
-            
       <div className="mission__block2">
-                                
-        <Image className="mission__img" {...data.image} />
-              
+          <Image className="mission__img" {...data.image} />       
       </div>
-          
     </section>
   );
 }
