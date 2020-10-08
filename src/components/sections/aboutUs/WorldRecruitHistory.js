44377716a000cd14468191a794683b69969268bc
HaiYen1995 <yen@world-works.co.jp>
2020-10-08 11:14:23 +0900

    modify css

--- src/components/sections/aboutUs/WorldRecruitHistory.js
+++ src/components/sections/aboutUs/WorldRecruitHistory.js
@@ -2,6 +2,8 @@ import React from "react";
 import "./WorldRecruitHistory.css";
 import Title from "../../common/Title.js";
 import Image from "../../common/Image";
+import "../../common/Description.css";
+import Description from "../../common/Description";
 
 export default function WorldReruitHistory({ data }) {
   console.log("WorldReruitHistory", data.image);
@@ -9,7 +11,8 @@ export default function WorldReruitHistory({ data }) {
     <section className="worldrecruithistory">
       <div className="worldrecruithistory__block1">
         <Title data={data.title} />
-        <p className="worldrecruithistory__description">{data.description}</p>
+        <Description data={data.description} />
+        {/* <p className="worldrecruithistory__description">{data.description}</p> */}
       </div>
       <div className="worldrecruithistory__block2">
         <Image className="worldrecruithistory__img" {...data.image} />
