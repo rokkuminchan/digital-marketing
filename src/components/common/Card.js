ea28170e36d16d6d0b2212e256389d0f8d804738
Vien-world <vien@world-works.co.jp>
2020-10-08 15:52:44 +0900

    modified data ang Img in Card

--- src/components/common/Card.js
+++ src/components/common/Card.js
@@ -4,10 +4,8 @@ import Image from "./Image";
 
 export default function Card({ data }) {
   return (
-    <div className="card">
-      <div className="card__img">
-        <Image {...data.image} />
-      </div>
+    <div className="card">      
+      <Image className="card__img" {...data.image} />
       <div className="card__content">
         <h3>{data.title}</h3>
         <p>{data.description}</p>
