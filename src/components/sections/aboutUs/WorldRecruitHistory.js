7cf3d50d00b171be7fe70ba1a1cdedac2594cf98
Guava <linh.guava94@gmail.com>
2020-10-08 10:10:55 +0900

    update WorldRecruitHistory by Gatsby

--- src/components/sections/aboutUs/WorldRecruitHistory.js
+++ src/components/sections/aboutUs/WorldRecruitHistory.js
@@ -6,10 +6,14 @@ import Image from "../../common/Image";
 export default function WorldReruitHistory({ data }) {
   console.log("WorldReruitHistory", data.image);
   return (
-    <section className="worldrecruitristory">
-      <Title data={data.title} />
-      <p className="worldrecruitristory__description">{data.description}</p>
-      <Image className="worldrecruitristory__img" {...data.image} />
+    <section className="worldrecruithistory">
+      <div className="worldrecruithistory__block1">
+        <Title data={data.title} />
+        <p className="worldrecruithistory__description">{data.description}</p>
+      </div>
+      <div className="worldrecruithistory__block2">
+        <Image className="worldrecruithistory__img" {...data.image} />
+      </div>
     </section>
   );
 }
