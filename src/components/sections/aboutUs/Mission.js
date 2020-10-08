0ba5b1a8f34480d75f48d77840f1d6b2642f2795
tuanpham95 <nevergiveup1195@gmail.com>
2020-10-08 20:16:55 +0900

    Update Mission.js

--- src/components/sections/aboutUs/Mission.js
+++ src/components/sections/aboutUs/Mission.js
@@ -7,20 +7,21 @@ import Image from "../../common/Image";
 export default function Mission({ data }) {
   return (
     <section className="mission">
+
       <div className="mission__block1">
-        <Title data={data.title} />
-        {/* <h3 className="mission__title">{data.title}</h3> */}
-        {/* <p className="mission__description">{data.description}</p> */}
-        <Description data={data.description} />
+
+        <Title className="mission__title" data={data.title} />
+
+        <Description className="mission__description" data={data.description} />
+
       </div>
+
       <div className="mission__block2">
-        {/* <img
-          className="mission__img"
-          src={data.image.src}
-          alt={data.image.alt}
-        ></img> */}
-        <Image {...data.image} />
+
+        <Image className="mission__img" {...data.image} />
+
       </div>
+
     </section>
   );
 }
