1e83d75e59dd697780c0e023c1d62b3b8422e541
VuongTran0104 <vuong@world-works.co.jp>
2020-10-09 10:10:07 +0900

    Update Vision js and css

--- src/components/sections/aboutUs/Vision.js
+++ src/components/sections/aboutUs/Vision.js
@@ -2,10 +2,14 @@ import React from "react";
 import "./Vision.css";
 
 export default function Vision({ data }) {
+    const backgroundImage = {
+    background: `url(${data.image.src}) center center / cover no-repeat`
+  };
+
   return (
     <section className="vision">
       <div>
-        <div className="vision-header">
+        <div className="vision-header" style={backgroundImage}>
           <div className="vision__title">
             <p>{data.title}</p>
           </div>
