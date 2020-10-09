f80031dbab8f8050e8a909f234ae0a99780c367c
VuongTran0104 <vuong@world-works.co.jp>
2020-10-09 17:43:38 +0900

    fix technologyList

--- src/components/sections/technology/TechnologyList.js
+++ src/components/sections/technology/TechnologyList.js
@@ -1,19 +1,27 @@
 import React from "react";
 import "./TechnologyList.css";
 import ToolCard from "../../common/ToolCard.js";
-import Title from "../../common/Title";
 import Description from "../../common/Description";
 
 export default function TechnologyList({ data }) {
+  const titleLarge = data.title.replace('リスト','');
+  const titleSmall = data.title.replace('技術','');
   return (
-    <section className="technologylist">
+    <section className="technology-list">
       <div>
-        <Title data={data.title} />
-        <Description data={data.description} />
+        <div className="technology-list__header">
+          <div className="technology-list__title">
+            <span className="technology-list__title-large">{titleLarge}</span>
+            <span className="technology-list__title-small">{titleSmall}</span>
+          </div>
+          <Description data={data.description} />
+        </div>
         <div className="technologylist__items">
-          {data.items.map((list, index) => {
-            <ToolCard key={index} data={list} />;
-          })}
+          {
+          data.item.map((list, index) => (
+            <ToolCard key={index} data={list} />
+          ))
+          }
         </div>
       </div>
     </section>
