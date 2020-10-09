5385fb2b32eb6990804d000ee3b6951ef69998c6
rokku <loc@world-works.co.jp>
2020-10-09 16:11:58 +0900

    Remove img folder and simplify getting data in Techonoly page

--- src/components/pages/Technology.js
+++ src/components/pages/Technology.js
@@ -2,7 +2,11 @@ import React from "react";
 import Layout from "../layout";
 import PageHeader from "../sections/technology/PageHeader";
 
-export default function Technology(props) {
+export default function Technology({ data }) {
+  // Chạy xem trong console chrome xem dữ liệu nó ra như thế nào rồi truyền vào
+  // Xong rồi xoá dòng này giúp anh nhé Tuấn
+  console.log(data.technologyJson);
+
   return (
     <Layout>
       <PageHeader />
