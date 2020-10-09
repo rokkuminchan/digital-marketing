c08323b1d42e3cd6f9e72f37505c0bd4d2648624
rokku <loc@world-works.co.jp>
2020-10-09 12:18:32 +0900

    Asign data to technology page

--- src/components/pages/Technology.js
+++ src/components/pages/Technology.js
@@ -1,6 +1,7 @@
 import React from "react";
 import Layout from "../layout";
 
-export default function Technology(props) {
+export default function Technology({ data }) {
+  console.log(data.technologyJson);
   return <Layout></Layout>;
 }
