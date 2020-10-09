165025230b424049bf9082b79bcc000a5270d99e
tuanpham95 <nevergiveup1195@gmail.com>
2020-10-09 15:18:32 +0900

    Update PageHeader.js

--- src/components/sections/technology/PageHeader.js
+++ src/components/sections/technology/PageHeader.js
@@ -1,12 +1,16 @@
 import React from "react";
-import Image from "../../common/Image";
-import "./PageHeader.css";
-export default function PageHeader({ data }) {
+import "./PageHeader1.css";
+import img from "../../../images/Technology/technology-pageheader.png";
+export default function PageHeader() {
   return (
-    <section className="pageheader">
-      <h1 className="page-header__title">{data.title}</h1>
-      <Image className="page-header__img" {...data.image} />
-      <p className="page-header__description">{data.description}</p>
+    <section className="tech-page-header">
+      <h1 className="tech-page-header__title">技術</h1>
+      <div className="tech-page-header__img">
+        <img src={img} className="tech-image" />
+      </div>
+      <p className="tech-page-header__description">
+        ITチーム全員が最新のテクノロジーに習熟しています。オープンソース、オープンスタンダードのツールを選択し、持続性の高い開発を実現します。性能や生産性そして品質に違いが出ます
+      </p>
     </section>
   );
 }
