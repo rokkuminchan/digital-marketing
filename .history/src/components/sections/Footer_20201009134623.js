5945d5bb3429f3eea6481997fd47d61c60528ccc
vuduchuy94 <vuduchuybk@gmail.com>
2020-10-09 14:10:32 +0900

    edit style footer

--- .history/src/components/sections/Footer_20201009134623.js
+++ .history/src/components/sections/Footer_20201009134623.js
@@ -7,9 +7,8 @@ export default function Footer() {
       <div className="footer__left">
         <h3>WORLD WORKS</h3>
         <p className="footer__links">
-          <a href="#"> ホーム </a>｜｜<a href="#">サービス</a>｜｜{" "}
-          <a href="#">技術</a>｜｜
-          <a href="#">お問い合わせ</a>｜｜<a href="#">我々について</a>
+          <a href="#"> ホーム </a>｜｜<a href="#">サービス</a>｜｜
+          <a href="#">技術</a>｜｜<a href="#">我々について</a>
         </p>
       </div>
 
