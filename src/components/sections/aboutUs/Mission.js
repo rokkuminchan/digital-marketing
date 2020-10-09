fbd9b88dc6940d843ba86b1a040e09808692f4e8
HaiYen1995 <yen@world-works.co.jp>
2020-10-10 00:51:39 +0900

    modify file css

--- src/components/sections/aboutUs/Mission.js
+++ src/components/sections/aboutUs/Mission.js
@@ -1,27 +1,27 @@
 import React from "react";
 import "./Mission.css";
-import Title from "../../common/Title";
-import Description from "../../common/Description";
+// import Title from "../../common/Title";
+// import Description from "../../common/Description";
 import Image from "../../common/Image";
 
 export default function Mission({ data }) {
   return (
-    <section className="mission">
+    <section>
+      <div className="mission">
+        <div className="mission__block1">
+          {/* <Title className="mission__title" data={data.title} />
+          <Description
+            className="mission__description"
+            data={data.description}
+          /> */}
+          <h3 className="mission__title">{data.title}</h3>
+          <p className="mission__description">{data.description}</p>
+        </div>
 
-      <div className="mission__block1">
-
-        <Title className="mission__title" data={data.title} />
-
-        <Description className="mission__description" data={data.description} />
-
+        <div className="mission__block2">
+          <Image className="mission__img" {...data.image} />
+        </div>
       </div>
-
-      <div className="mission__block2">
-
-        <Image className="mission__img" {...data.image} />
-
-      </div>
-
     </section>
   );
 }
