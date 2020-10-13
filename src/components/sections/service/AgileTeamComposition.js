d690fff0f14c997d16f8ec53db69b3aa1ec1ac17
ducva <duc@world-works.co.jp>
2020-10-13 15:07:53 +0900

    add Component AgileTeamComposition

---
+++ src/components/sections/service/AgileTeamComposition.js
@@ -0,0 +1,11 @@
+import React from "react"
+import "./AgileTeamComposition.css"
+
+const AgileTeamComposition = ({ data }) => {
+    return (
+        <div className ="">
+
+        </div>
+    );
+}
+export default AgileTeamComposition;
