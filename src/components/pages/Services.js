6614dd969f07c017dc6bbb6b200628d69e2849f0
Vu Sinh <sinh@world-works.co.jp>
2020-10-14 10:05:38 +0900

    add webapprequirment section

--- src/components/pages/Services.js
+++ src/components/pages/Services.js
@@ -1,12 +1,14 @@
 import React from "react";
 import Layout from "../layout";
 import ServicePageHeader from "../../components/sections/service/ServicePageHeader";
+import WebAppRequirment from "../../components/sections/service/WebAppRequirement"
 import AgileTeamComposition from "../../components/sections/service/agileTeamComposition";
 export default function Services({ data }) {
   console.log(data.servicesJson);
   return (
     <Layout>
       <ServicePageHeader data={data.servicesJson.ServiceHeader} />
+      <WebAppRequirment data={data.servicesJson.WebApplicationRequirment} />
       <AgileTeamComposition data={data.servicesJson.AgileTeamComposition}/>
     </Layout>
   );
