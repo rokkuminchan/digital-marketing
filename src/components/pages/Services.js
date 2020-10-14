25bd8e5702992239c26b03a22770d51e5d1d0e5f
unknown <vuong@world-works.co.jp>
2020-10-14 11:22:33 +0900

    update services.js

--- src/components/pages/Services.js
+++ src/components/pages/Services.js
@@ -1,14 +1,16 @@
 import React from "react";
 import Layout from "../layout";
 import ServicePageHeader from "../../components/sections/service/ServicePageHeader";
-import WebAppRequirment from "../../components/sections/service/WebAppRequirement"
+import WebApplication from "../../components/sections/service/WebApplication";
+import WebAppRequirment from "../../components/sections/service/WebAppRequirement";
 import AgileTeamComposition from "../../components/sections/service/agileTeamComposition";
 export default function Services({ data }) {
   console.log(data.servicesJson);
   return (
     <Layout>
       <ServicePageHeader data={data.servicesJson.ServiceHeader} />
-      <WebAppRequirment data={data.servicesJson.WebApplicationRequirment} />
+      <WebApplication data={data.servicesJson.WebApplication} />
+      <WebAppRequirment data={data.servicesJson.WebApplicationRequirment} />     
       <AgileTeamComposition data={data.servicesJson.AgileTeamComposition}/>
     </Layout>
   );
