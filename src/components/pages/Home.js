d5a9784a9acb239f969b6b942e8c1db1a4ddd805
unknown <vuong@world-works.co.jp>
2020-10-15 14:52:26 +0900

    update WebApplication Home

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -1,12 +1,14 @@
 import React from "react";
 import Layout from "../layout";
-import DetailedWork from '../sections/home/DetailedWork'
-import TeamCharacteristic from '../sections/home/TeamCharacteristic'
-import Technology from '../sections/home/Technology'
+import DetailedWork from '../sections/home/DetailedWork';
+import TeamCharacteristic from '../sections/home/TeamCharacteristic';
+import Technology from '../sections/home/Technology';
+import WebApplication from '../sections/home/WebApplication';
 
 export default function Home({data}) {
   return (
     <Layout>
+      <WebApplication data={data.WebApplication} />
       <DetailedWork data={data.DetailedWork} />
       <TeamCharacteristic data={data.TeamCharacteristic} />
       <Technology data={data.Technology} />
