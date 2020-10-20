72a19196615f5d900d08ed5f61665926d8f913a7
vuduchuy94 <vuduchuybk@gmail.com>
2020-10-20 14:41:01 +0900

    edit contact

--- src/App.js
+++ src/App.js
@@ -4,15 +4,16 @@ import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 // Components
 import Home from "./components/pages/Home";
-import AboutUs from "./components/pages/AboutUs";
+import Services from "./components/pages/Services";
 import Technology from "./components/pages/Technology";
-import Services from "./components/pages/Services"
-
+import AboutUs from "./components/pages/AboutUs";
+import Contact from "./components/pages/Contact";
 // Data
 import ourTeamJsonData from "./data/ourTeam/ourTeamData.json";
-import technologyJsonData from "./data/technology/technologyData.json"
+import technologyJsonData from "./data/technology/technologyData.json";
 import serviceJsonData from "./data/service/serviceData.json";
 import homeJsonData from "./data/home/homeData.json";
+import contactJsonData from "./data/contact/contactData.json";
 
 function App() {
   return (
@@ -22,14 +23,17 @@ function App() {
           <Route exact path="/">
             <Home data={homeJsonData} />
           </Route>
-          <Route exact path="/about-us">
-            <AboutUs data={ourTeamJsonData} />
+          <Route exact path="/services">
+            <Services data={serviceJsonData} />
           </Route>
           <Route exact path="/technology">
             <Technology data={technologyJsonData} />
           </Route>
-          <Route exact path="/services">
-            <Services data={serviceJsonData} />
+          <Route exact path="/about-us">
+            <AboutUs data={ourTeamJsonData} />
+          </Route>
+          <Route exact path="/contact">
+            <Contact data={contactJsonData} />
           </Route>
         </Switch>
       </Router>
