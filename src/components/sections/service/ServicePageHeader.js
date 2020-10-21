6abc723e1252105a539234a9206b4f369c767121
rokku <loc@world-works.co.jp>
2020-10-21 13:23:01 +0900

    Styling services header

--- src/components/sections/service/ServicePageHeader.js
+++ src/components/sections/service/ServicePageHeader.js
@@ -1,20 +1,21 @@
 import React from "react";
-import "./ServicePageHeader.css";
 import Image from "../../common/Image";
+import "./ServicePageHeader.css";
+
 export default function SerivePageHeader({ data }) {
   return (
     <section className="service-page-header">
-      <div className="service-page-header__block1">
-        <h1 className="service-page-header__title">{data.title} </h1>
-        <div className="service-page-header__block2">
-          <p className="service-page-header__description">{data.description}</p>
-        </div>
-        <div className="service-page-header__img">
-          <Image className="service-img" {...data.image} />
-        </div>
+      <div className="service-page-header__content">
+        <h2 className="service-page-header__title">{data.title}</h2>
+        <p className="service-page-header__description">
+          {data.description}
+        </p>
       </div>
-      <div className="service-page-header__block3">
-        <p className="service-page-header__subDescription">
+
+      <Image className="service-page-header__img" {...data.image} />
+      
+      <div className="service-page-header__sub-description-wrapper">
+        <p className="service-page-header__sub-description">
           {data.subDescription}
         </p>
       </div>
