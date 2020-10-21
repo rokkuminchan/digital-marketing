0954970937b73a6c639e53fae511fd5c2416adf5
haiworldworks <hai@world-works.co.jp>
2020-10-21 11:38:33 +0900

    Home - WebAppForDigital data update 201021-1

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -4,10 +4,12 @@ import DetailedWork from "../sections/home/DetailedWork";
 import TeamCharacteristic from "../sections/home/TeamCharacteristic";
 import Technology from "../sections/home/Technology";
 import WebApplication from "../sections/home/WebApplication";
+import WebAppForDigital from "../sections/home/WebAppForDigital"
 export default function Home({ data }) {
   return (
     <Layout>
       <WebApplication data={data.WebApplication} />
+      <WebAppForDigital data={data.WebAppForDigital}/>
       <DetailedWork data={data.DetailedWork} />
       <TeamCharacteristic data={data.TeamCharacteristic} />
       <Technology data={data.Technology} />
