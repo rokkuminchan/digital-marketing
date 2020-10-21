187178305f7bad12010e704adcd7997bdd19557b
Vu Sinh <sinh@world-works.co.jp>
2020-10-21 16:21:20 +0900

    update Home data(About us section)

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -5,12 +5,15 @@ import TeamCharacteristic from "../sections/home/TeamCharacteristic";
 import Technology from "../sections/home/Technology";
 import WebApplication from "../sections/home/WebApplication";
 import WebAppForDigital from "../sections/home/WebAppForDigital"
+import AboutUs from "../sections/home/AboutUs"
+
 export default function Home({ data }) {
   return (
     <Layout>
       <WebApplication data={data.WebApplication} />
       <WebAppForDigital data={data.WebAppForDigital}/>
       <DetailedWork data={data.DetailedWork} />
+      <AboutUs data={data.Watashitachi} />
       <TeamCharacteristic data={data.TeamCharacteristic} />
       <Technology data={data.Technology} />
     </Layout>
