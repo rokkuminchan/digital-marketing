a1d39050ef94165e1c322f3e6edbf8b62888a8ca
haiworldworks <hai@world-works.co.jp>
2020-10-21 12:31:49 +0900

    Home - WebAppForDigital mobile css  201021-2

--- src/components/sections/home/WebAppForDigital.js
+++ src/components/sections/home/WebAppForDigital.js
@@ -1,19 +1,22 @@
 import React from 'react';
-import Title from '../../common/Title'
-import Description from '../../common/Description'
-import Image from '../../common/Image'
+import Title from '../../common/Title';
+import Description from '../../common/Description';
+import Image from '../../common/Image';
+import './WebAppForDigital.css';
 
 const WebAppForDigital = ({data}) => {
     return(
         <section className = "webAppForDigital">
+            <div className="webAppForDigital__wrapper">
             <div className = "webAppForDigital__title">
                 <Title data = {data.title} />
             </div>
             <div className = "webAppForDigital__image">
-                <Image {...data.image}/>
+                <Image  className="digital-image" {...data.image}/>
             </div>
-            <div className = "webAppForDigital__description">
+            <div className = "webAppForDigital__desc">
                 <Description data = {data.description} />
+            </div>
             </div>
         </section>
     )
