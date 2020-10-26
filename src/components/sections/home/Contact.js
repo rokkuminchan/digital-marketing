4946e6f59a48ae233a289b5012b2706832dc1366
vuduchuy94 <vuduchuybk@gmail.com>
2020-10-26 11:58:34 +0900

    renamed contact

--- src/components/sections/home/Contact.js
+++ src/components/sections/home/Contact.js
@@ -1,17 +1,17 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
-import "./Button.css";
+import "./Contact.css";
 
-const Button = () => {
+const Contact = () => {
   return (
-    <div className="button-contain">
+    <section className="button-contain">
       <button className="button">
         <Link to="/contact" className="button__link">
           お問い合わせ
         </Link>
       </button>
-    </div>
+    </section>
   );
 };
-export default Button;
+export default Contact;
