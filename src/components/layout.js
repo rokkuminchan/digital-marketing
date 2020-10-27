3d7f160a024ecc2b4ec67a6dbc9e1f372d381acb
rokku <loc@world-works.co.jp>
2020-10-27 13:53:19 +0900

    Add favicon and metadata data to page

--- src/components/layout.js
+++ src/components/layout.js
@@ -9,7 +9,12 @@ export default function Layout(props) {
   return (
     <React.Fragment>
       <Helmet>
+        <meta property="og:title" content=" ワールドのITチーム" />     
+        <meta property="og:description" content="ワールドのITチームのご紹介サイト" />     
+        <meta property="og:site_name" content="WORLD IT TEAM" />     
+        <meta property="og:image" content=" https://lh3.googleusercontent.com/proxy/Rhy7Lon3zF3xRVs2rKaazuE4Nn0ckT41xYIxsmDlmMRWV47-034U5bCLkNCxa__uSefPcfnlPer07lcxe0Lm2uKpolsGHem-JWoWw-HlOA1Zrd2GfQw52yqHHVy_" />
         <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/font-awesome/4.2.0/css/font-awesome.min.css"></link>
+        <title>ワールドのITチーム</title>
       </Helmet>
       <MenuHeader />
         <main>{props.children}</main>
