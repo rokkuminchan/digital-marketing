1c1bf58f0e72ebf183f4db7c1a1dce1b0d819dc5
thainguyen0109 <71751911+thainguyen0109@users.noreply.github.com>
2020-11-19 08:50:17 +0900

    RecruitHomeHeaderモバイル

--- src/components/pages/RecruitHome.js
+++ src/components/pages/RecruitHome.js
@@ -1,10 +1,12 @@
 import React from "react";
+import RecruitHomeHeader from "../sections/recruit/home/RecruitHomeHeader";
 
 import RecruitHomeRecruitmentJobs from "../sections/recruit/home/RecruitHomeRecruitmentJobs";
 
 export default function RecruitHome({ data }) {
   return (
     <React.Fragment>
+      <RecruitHomeHeader/>
       <RecruitHomeRecruitmentJobs data={data.RecruitHomeRecruitmentJobs} />
     </React.Fragment>
   );
---
+++ src/components/sections/recruit/home/RecruitHomeHeader.js
@@ -0,0 +1,28 @@
+import React from "react";
+import "./RecruitHomeHeader.css";
+import Background from "../../../../images/recruit_home__banner.jpeg";
+
+export default function RecruitHomeHeader() {
+  return (
+    <section className="recruit-home-header">
+      <div className="recruit-home-header__banner">
+        <div
+          style={{
+            background: `transparent url(${Background})`,
+            "background-size": `cover`,
+            "background-repeat": `no-repeat`,
+            "background-position": `90% 100% `
+          }}
+          className="recruit-home-header__img"
+        ></div>
+        <div className="recruit-home-header__content">
+          <h2 style={{ opacity: 1 }} className="recruit-home-header__title">Recruit</h2>
+          <p style={{ opacity: 1 }} className="recruit-home-header__description">
+          World IT Team は、「チームワーク」を大切にしています。高度ＩＴ人材として日本のＤＸに貢献しましょう
+          </p>
+        </div>
+      </div>
+    </section>
+  );
+}
+
