9218e9ee4dece0f54c342e6cbe1f31a405603545
unknown <vuong@world-works.co.jp>
2020-11-18 13:02:45 +0900

    update recruitHome Data

--- src/components/pages/RecruitHome.js
+++ src/components/pages/RecruitHome.js
@@ -1,11 +1,11 @@
 import React from "react";
 
+import RecruitHomeRecruitmentJobs from "../sections/recruit/home/RecruitHomeRecruitmentJobs";
+
 export default function RecruitHome({ data }) {
-  return <React.Fragment>
-
-
-
-
-    
-  </React.Fragment>;
+  return (
+    <React.Fragment>
+      <RecruitHomeRecruitmentJobs data={data.RecruitHomeRecruitmentJobs} />
+    </React.Fragment>
+  );
 }
--- src/components/sections/recruit/home/RecruitHomeRecruitmentJobs.js
+++ src/components/sections/recruit/home/RecruitHomeRecruitmentJobs.js
@@ -1,3 +1,24 @@
 import React from "react";
+import { Button } from "@material-ui/core";
 
-export default function Recrui
+export default function RecruitHomeRecruitmentJobs({ data }) {
+  return (
+    <section className="recruit-home-recruitment-jobs">
+      {data.map((item, index) => (
+        <div className="recruit-home-recruitment-jobs__cards" key={index}>
+          <div className="recruit-home-recruitment-jobs__cards-image">
+            <div className="recruit-home-recruitment-jobs__cards-image-title">
+              {item.title}
+            </div>
+          </div>
+          <p className="recruit-home-recruitment-jobs__description">
+            {item.description}
+          </p>
+          <Button variant="contained" color="primary">
+            {item.button.title}
+          </Button>
+        </div>
+      ))}
+    </section>
+  );
+}
