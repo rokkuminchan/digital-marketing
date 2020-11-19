4a863d69600d9a29427c25aeac58657049dbe671
hoang vuong <hoangvuong@MacBook-Pro-cua-hoang.local>
2020-11-19 18:34:27 +0900

    edit css jobOther

--- src/components/sections/recruit/common/jobOther.js
+++ src/components/sections/recruit/common/jobOther.js
@@ -2,7 +2,7 @@ import React from "react";
 
 import "./jobOther.css";
 import Title from "../../../common/Title";
-import ALink from '../../../common/ALink';
+import {Link} from "react-router-dom";
 
 export default function RecruitJobOther({ data }) {
   return (
@@ -13,7 +13,7 @@ export default function RecruitJobOther({ data }) {
         <div className="recruit-jobOther__content">
             {data.content.map((item, index) => (
                 <div className="recruit-jobOther__link" key={index}>
-                    <ALink to={item.link}>{item.link.name}</ALink>
+                    <Link to={item.link.src}>{item.link.name}</Link>
                 </div>
              ))}
       </div>
