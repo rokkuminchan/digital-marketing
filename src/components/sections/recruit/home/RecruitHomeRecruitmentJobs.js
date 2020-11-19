8d3f4837301b37b98cec0dcc892556571be45220
unknown <vuong@world-works.co.jp>
2020-11-19 10:20:47 +0900

    update recruitHome Recruitment Jobs

--- src/components/sections/recruit/home/RecruitHomeRecruitmentJobs.js
+++ src/components/sections/recruit/home/RecruitHomeRecruitmentJobs.js
@@ -1,22 +1,33 @@
 import React from "react";
+import "./RecruitHomeRecruitmentJobs.css";
 import { Button } from "@material-ui/core";
+import Image from "../../../common/Image";
 
 export default function RecruitHomeRecruitmentJobs({ data }) {
   return (
     <section className="recruit-home-recruitment-jobs">
       {data.map((item, index) => (
-        <div className="recruit-home-recruitment-jobs__cards" key={index}>
-          <div className="recruit-home-recruitment-jobs__cards-image">
-            <div className="recruit-home-recruitment-jobs__cards-image-title">
-              {item.title}
-            </div>
+        <div className="recruit-home-recruitment-jobs__card" key={index}>
+          <Image
+            className="recruit-home-recruitment-jobs__card-image"
+            {...item.image}
+          />
+          <div className="recruit-home-recruitment-jobs__card-footer">
+            <div
+              className="recruit-home-recruitment-jobs__card-title"
+              dangerouslySetInnerHTML={{ __html: item.title }}
+            ></div>
+            <p className="recruit-home-recruitment-jobs__card-description">
+              {item.description}
+            </p>
+            <Button
+              variant="contained"
+              color="primary"
+              className="recruit-home-recruitment-jobs__card-button"
+            >
+              {item.button.title}
+            </Button>
           </div>
-          <p className="recruit-home-recruitment-jobs__description">
-            {item.description}
-          </p>
-          <Button variant="contained" color="primary">
-            {item.button.title}
-          </Button>
         </div>
       ))}
     </section>
