17b46bbc09ba047babda47bae6103489344463ca
Vu Sinh <sinh@world-works.co.jp>
2020-11-19 18:18:53 +0900

    update polici common section style

--- src/components/pages/Backend.js
+++ src/components/pages/Backend.js
@@ -1,10 +1,14 @@
 import React from "react";
+import Breadcrumb from "../sections/recruit/common/Breadcrumb"
+import BackendPageHeader from "../sections/recruit/backend/BackendPageHeader"
 import Policy from "../sections/recruit/common/Policy";
 
 export default function Backend({data}){
     return (
     <React.Fragment>
-      <Policy data = {data.DescriptiveIcon}/>
+     <BackendPageHeader data={data.Header} />
+     <Breadcrumb data={data.Header}/>
+      <Policy data = {data.Policy}/>
     </React.Fragment>
     );
 }
