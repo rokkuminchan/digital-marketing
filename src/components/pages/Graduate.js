e5fdc300112f788e20e57c8c8dcf97f40cad6776
VoTanTruong <truong@world-works.co.jp>
2020-11-20 15:57:10 +0900

    add data graduate

--- src/components/pages/Graduate.js
+++ src/components/pages/Graduate.js
@@ -1,13 +1,17 @@
 import React from "react";
 import GraduateHeader from "../sections/recruit/new-graduate/GraduateHeader";
 import RecruitDetail from "../common/RecruitDetail";
+import Breadcrumb from "../sections/recruit/common/Breadcrumb"
+import RecruitJobOther from '../sections/recruit/common/JobOther';
 
 
 export default function Graduate({data}){
     return (
     <React.Fragment>
       <GraduateHeader data={data.graduateHeader} />
+      <Breadcrumb data={data.graduateHeader}/>
       <RecruitDetail data={data.recruitDetails}/>
+      <RecruitJobOther data={data.JobOther}/>
     </React.Fragment>
     );
 }
