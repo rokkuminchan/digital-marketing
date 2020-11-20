8d9c6ccc5a175424baf355c495f0f5b2d8655fbb
ducva9892 <ducva9892@users.noreply.github.com>
2020-11-20 19:02:48 +0900

    add Section Director Page

--- src/components/pages/Director.js
+++ src/components/pages/Director.js
@@ -1,7 +1,11 @@
 import React from "react";
 import DirectorPageHeader from "../sections/recruit/director/DirectorPageHeader"
-import JobDescription from "../sections/recruit/director/JobDescription";
+import JobDescription from "../sections/recruit/common/JobDescription";
 import Breadcrumb from "../sections/recruit/common/Breadcrumb"
+import TargetPerson from "../sections/recruit/common/TargetPerson"
+import Policy from "../sections/recruit/common/Policy"
+import RecruitJobOther from "../sections/recruit/common/JobOther";
+
 
 export default function Director({ data }) {
   
@@ -10,6 +14,9 @@ export default function Director({ data }) {
       <DirectorPageHeader data={data.Header}/>
       <Breadcrumb data={data.Header}/>
       <JobDescription data ={data.JobDescription}/>
+      <TargetPerson data = {data.TargetPerson} />
+      <Policy data = {data.Policy}/>
+      <RecruitJobOther data={data.JobOther}/>
   </React.Fragment>
   );
 }
