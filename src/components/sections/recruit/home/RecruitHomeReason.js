6804a48229121598f760c734447045bbb145255d
unknown <vuong@world-works.co.jp>
2020-11-20 14:47:10 +0900

    update Home Reason

--- src/components/sections/recruit/home/RecruitHomeReason.js
+++ src/components/sections/recruit/home/RecruitHomeReason.js
@@ -2,24 +2,28 @@ import React from "react";
 import "./RecruitHomeReason.css";
 import Image from "../../../common/Image";
 
-export default function RecruitHomeReason({data}){
-    return(
-        <section className="recruit-home-reason">
-            <h3 className="recruit-home-reason-title">{data.title}</h3>
-            <div className="recruit-home-reason__items">
-                {data.items.map((item, index) => (
-                    <div key={index} className="recruit-home-reason__card">
-                        <Image className="recruit-home-reason__card-image" {...item.image}/>
-                        <div className="recruit-home-reason__card-content">
-                            {/* <h3>{item.title}</h3> */}
-                            <div className="recruit-home-reason__card-content-title">{item.title.split("\n").map((t, key)=>{
-                                return <div key={key}>{t}</div>
-                            })}</div>
-                            <p>{item.description}</p>
-                        </div>
-                    </div>  
-                ))}
+export default function RecruitHomeReason({ data }) {
+  return (
+    <section className="recruit-home-reason">
+      <h3 className="recruit-home-reason-title">{data.title}</h3>
+      <div className="recruit-home-reason__items">
+        {data.items.map((item, index) => (
+          <div key={index} className="recruit-home-reason__card">
+            <Image
+              className="recruit-home-reason__card-image"
+              {...item.image}
+            />
+            <div className="recruit-home-reason__card-content">
+              {/* <h3>{item.title}</h3> */}
+              <div
+                className="recruit-home-reason__card-content-title"
+                dangerouslySetInnerHTML={{ __html: item.title }}
+              ></div>
+              <p>{item.description}</p>
             </div>
-        </section>
-    )
+          </div>
+        ))}
+      </div>
+    </section>
+  );
 }
--- src/components/sections/recruit/home/RecruitHomeTargetingPersons.js
+++ src/components/sections/recruit/home/RecruitHomeTargetingPersons.js
@@ -8,13 +8,16 @@ export default function RecruitHomeTargetingPersons({ data }) {
   return (
     <section className="recruit-home-targeting-persons">
       <Title data={data.title} />
-      <Image
-        {...data.image}
-        className="recruit-home-targeting-persons__image"
-      />
+      <div className="recruit-home-targeting-persons__image--wrapper">
+        <Image
+          {...data.image}
+          className="recruit-home-targeting-persons__image"
+        />
+      </div>
       <div className="recruit-home-targeting-persons__items--wrapper">
         {data.items.map((item1, index) => (
           <div className="recruit-home-targeting-persons__items" key={index}>
+            <div className="recruit-home-targeting-persons__conner-top"></div>
             <div className="recruit-home-targeting-persons__items-title">
               {item1.title}
             </div>
@@ -36,6 +39,7 @@ export default function RecruitHomeTargetingPersons({ data }) {
           </div>
         ))}
       </div>
+      <div class="recruit-home-targeting-persons__clear"></div>
     </section>
   );
 }
