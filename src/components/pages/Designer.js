af18e970a00b30dea957425e2e178feeff766c99
hoang vuong <hoangvuong@MacBook-Pro-cua-hoang.local>
2020-11-20 16:23:02 +0900

    edit file Designer.js and json data

--- src/components/pages/Designer.js
+++ src/components/pages/Designer.js
@@ -1,9 +1,19 @@
 import React from "react";
+
 import RecruitJobOther from "../sections/recruit/common/JobOther";
+import Breadcrumb from "../sections/recruit/common/Breadcrumb";
+import FrontTargetPerson from "../sections/recruit/frontend/FrontTargetPerson";
+import Policy from "../sections/recruit/common/Policy";
+import JobPageHeader  from "../sections/recruit/common/JobPageHeader";
+
 
 export default function Designer({ data }) {
   return (
   <React.Fragment>
+      <JobPageHeader data= {data.designer.Pageheader}/>
+      <Breadcrumb data={data.designer.Pageheader}/>
+      <FrontTargetPerson data={data.designer.TargetPerson} />
+      <Policy data = {data.designer.Policy}/>
       <RecruitJobOther data={data.designer.JobOther}/>
   </React.Fragment>
   );
