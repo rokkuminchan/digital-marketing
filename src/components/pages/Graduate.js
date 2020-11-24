6e6b0ecde78e2a5d79bfafb8e11d8c569f18a105
haiman232 <haiman.jp@gmail.com>
2020-11-24 17:55:18 +0900

    Job Details code update 5

--- src/components/pages/Graduate.js
+++ src/components/pages/Graduate.js
@@ -3,6 +3,7 @@ import JobPageHeader from "../sections/recruit/common/JobPageHeader";
 import Breadcrumb from "../sections/recruit/common/Breadcrumb";
 import RecruitDetail from "../sections/recruit/common/RecruitDetail";
 import RecruitJobOther from "../sections/recruit/common/JobOther";
+import EntryButton from "../common/EntryButton";
 
 
 export default function Graduate({data}){
@@ -12,6 +13,7 @@ export default function Graduate({data}){
       <Breadcrumb data={data.graduateHeader}/>
       <RecruitDetail data={data.JobDetails} />
       <RecruitJobOther data={data.JobOther} />
+      <EntryButton path="./entry" />
     </React.Fragment>
     );
 }
