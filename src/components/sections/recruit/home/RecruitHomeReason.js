d4794c333c1d6579b4325d4f7f664e27e9eed08e
unknown <vuong@world-works.co.jp>
2020-11-25 13:00:19 +0900

    update translate for recruit home

--- src/components/sections/recruit/home/RecruitHomeReason.js
+++ src/components/sections/recruit/home/RecruitHomeReason.js
@@ -1,11 +1,13 @@
 import React from "react";
 import "./RecruitHomeReason.css";
+import { useTranslation } from "react-i18next";
 import Image from "../../../common/Image";
 
 export default function RecruitHomeReason({ data }) {
+  const { t } = useTranslation();
   return (
     <section className="recruit-home-reason">
-      <h3 className="recruit-home-reason-title">{data.title}</h3>
+      <h3 className="recruit-home-reason-title">{t(data.title)}</h3>
       <div className="recruit-home-reason__items">
         {data.items.map((item, index) => (
           <div key={index} className="recruit-home-reason__card">
@@ -17,9 +19,9 @@ export default function RecruitHomeReason({ data }) {
               {/* <h3>{item.title}</h3> */}
               <div
                 className="recruit-home-reason__card-content-title"
-                dangerouslySetInnerHTML={{ __html: item.title }}
+                dangerouslySetInnerHTML={{ __html: t(item.title) }}
               ></div>
-              <p>{item.description}</p>
+              <p>{t(item.description)}</p>
             </div>
           </div>
         ))}
