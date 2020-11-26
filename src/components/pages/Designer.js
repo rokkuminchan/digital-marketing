334634e233b3d4004f32555e46ff029152bfeb25
haiman232 <haiman.jp@gmail.com>
2020-11-26 12:24:25 +0900

    Entry Button detail

--- src/components/pages/Designer.js
+++ src/components/pages/Designer.js
@@ -3,6 +3,7 @@ import React from "react";
 import RecruitJobOther from "../sections/recruit/common/JobOther";
 import Breadcrumb from "../sections/recruit/common/Breadcrumb";
 import RecruitDetail from "../sections/recruit/common/RecruitDetail";
+import DetailEntryButton from "../sections/recruit/common/DetailEntryButton"
 import JobPageHeader  from "../sections/recruit/common/JobPageHeader";
 import EntryButton from "../common/EntryButton";
 
@@ -13,8 +14,9 @@ export default function Designer({ data }) {
       <JobPageHeader data= {data.designer.Pageheader}/>
       <Breadcrumb data={data.designer.Pageheader}/>
       <RecruitDetail data={data.designer.JobDetails} />
+      <DetailEntryButton path="./designer/entry" />
       <RecruitJobOther data={data.designer.JobOther}/>
-      <EntryButton path="./entry" />
+      <EntryButton path="./designer/entry" />
   </React.Fragment>
   );
 }
