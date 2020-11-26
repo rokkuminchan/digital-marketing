83f9c7c6b72fa048f50e294ec3d8bec72dccd74b
VoTanTruong <truong@world-works.co.jp>
2020-11-26 18:09:26 +0900

    edit link ->alink detailentrybutton

--- src/components/sections/recruit/common/DetailEntryButton.js
+++ src/components/sections/recruit/common/DetailEntryButton.js
@@ -1,16 +1,17 @@
 import React from "react";
-import {Link} from 'react-router-dom';
+import ALink from "../../../common/ALink";
 import {useTranslation} from 'react-i18next';
 import "./DetailEntryButton.css";
 
 export default function DetailEntryButton(props) {
     const {t} = useTranslation();
     return (
-      // <a href={props.path} className="recruit-detail__entry">
-      //   {t('recruit-entry__button')}
-      // </a>
-      <Link to={props.path} className="recruit-detail__entry">
+    <ALink
+      className="recruit-detail__entry"
+      exact
+      to={props.path}
+      activeClassName="active">
       {t('recruit-entry__button')}
-    </Link>
+    </ALink>
     );
 }
