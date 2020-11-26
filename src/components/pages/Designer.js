e5aba07e0b8d2c878e9cb58842ff92c5dbb48517
hoang vuong <hoangvuong@MacBook-Pro-cua-hoang.local>
2020-11-26 13:03:47 +0900

    add DesignerHeader

--- src/components/pages/Designer.js
+++ src/components/pages/Designer.js
@@ -3,15 +3,15 @@ import React from "react";
 import RecruitJobOther from "../sections/recruit/common/JobOther";
 import Breadcrumb from "../sections/recruit/common/Breadcrumb";
 import RecruitDetail from "../sections/recruit/common/RecruitDetail";
-import DetailEntryButton from "../sections/recruit/common/DetailEntryButton"
-import JobPageHeader  from "../sections/recruit/common/JobPageHeader";
+import DetailEntryButton from "../sections/recruit/common/DetailEntryButton";
+import DesignerHeader  from "../sections/recruit/designer/DesignerHeader";
 import EntryButton from "../common/EntryButton";
 
 
 export default function Designer({ data }) {
   return (
   <React.Fragment>
-      <JobPageHeader data= {data.designer.Pageheader}/>
+      <DesignerHeader data= {data.designer.Pageheader}/>
       <Breadcrumb data={data.designer.Pageheader}/>
       <RecruitDetail data={data.designer.JobDetails} />
       <DetailEntryButton path="./designer/entry" />
