f6fe8999c1dbf00023d4c789ef3b35c16d78689a
VoTanTruong <truong@world-works.co.jp>
2020-11-27 14:46:49 +0900

    add translate about us

--- src/components/sections/aboutUs/VNUniversity.js
+++ src/components/sections/aboutUs/VNUniversity.js
@@ -3,13 +3,15 @@ import Title from "../../common/Title";
 import Description from "../../common/Description";
 import University from "./University";
 import "./VNUniversity.css";
+import {useTranslation} from 'react-i18next';
 
 const VNUniversity = ({ data }) => {
+  const {t} = useTranslation();
   return (
     <section className="vnUniversity">
       <div className="vnUniversity__container">
-        <Title data={data.title} />
-        <Description data={data.description} />
+        <Title data={t(data.title)} />
+        <Description data={t(data.description)} />
         <div className="vnUniversity__list">
           {data.items.map((university, index) => {
             return <University key={index} data={university} />;
