fe9a289d42c32e6d39bf2c21f1f618c9623e1110
Duc Underground <ducva9892@users.noreply.github.com>
2020-11-30 13:55:01 +0900

    Re-style AbouyUS TeamCharacteristic

--- src/components/sections/aboutUs/TeamCharacteristic.js
+++ src/components/sections/aboutUs/TeamCharacteristic.js
@@ -9,10 +9,13 @@ const TeamCharacteristic = ({ data }) => {
   return (
     <section className="team-characteristic">
         <div className="team-characteristic__imgbg">
-          <div className="team-characteristic__title">
-            <Title data={t(data.title)} />
+          <div className ="team-characteristic__solution"></div>
+          <div className="team-characteristic__text">
+            <div className="team-characteristic__text-wrap">
+              <Title className="team-characteristic__title" data={t(data.title)} />
+              <p className="team-characteristic__description">{t(data.description)}</p>  
+            </div>
           </div>
-          <p className="team-characteristic__description">{t(data.description)}</p>
       </div>
       <div className="team-characteristic__items">
         {data.items.map((item, index) => {
