af94b6d89f2cda29b3d68c17f080aff85d2c3c22
rokkuminchan <loc@world-works.co.jp>
2020-11-30 12:13:22 +0900

    Show entry button when detail entry button visible from view

--- src/components/common/EntryButton.js
+++ src/components/common/EntryButton.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { useTranslation } from "react-i18next";
 import ALink from "../common/ALink";
 import "./EntryButton.css";
@@ -6,9 +6,32 @@ import "./EntryButton.css";
 // import "./EntryButtonAnimation";
 
 export default function EntryButton(props) {
+  var [showEntryButton, setShowEntryButton] = useState("");
+
   const { t } = useTranslation();
+
+  useEffect(() => {
+    const entryButtonElement = document.getElementById(props.detailEntryButtonId);
+
+    typeof window !== undefined && (window.addEventListener('scroll', function () {
+      setShowEntryButton(isScrolledIntoView(entryButtonElement) ? "" : "entry-button__show")
+    }, false));
+  }, [])
+
+  function isScrolledIntoView(el) {
+    var rect = el.getBoundingClientRect();
+    var elemTop = rect.top;
+    var elemBottom = rect.bottom;
+
+    // Only completely visible elements return true:
+    var isVisible = (elemTop >= 0) && (elemBottom <= window.innerHeight);
+    // Partially visible elements return true:
+    //isVisible = elemTop < window.innerHeight && elemBottom >= 0;
+    return isVisible;
+  }
+
   return (
-    <ALink to={props.path} className="entrybutton">
+    <ALink to={props.path} id="entrybutton" className={`entrybutton ${showEntryButton}`}>
       {t("recruit-entry__button")}
     </ALink>
   );
