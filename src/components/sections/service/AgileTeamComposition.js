418db9ed705bd17d0d33bfa549a56a65a7fa7940
haiww <hai@world-works.co.jp>
2021-04-21 13:49:05 +0900

    Css refactor

--- src/components/sections/service/AgileTeamComposition.js
+++ src/components/sections/service/AgileTeamComposition.js
@@ -2,29 +2,33 @@ import React from "react";
 import "./AgileTeamComposition.css";
 import Title from "../../common/Title";
 import AgileTeamCompositionList from "./AgileTeamCompositionList";
-import {useTranslation} from 'react-i18next';
+import { useTranslation } from "react-i18next";
 
-
-const AgileTeamComposition = ({data}) =>{
-    const {t} = useTranslation();
-    return(
-        <section className="agileTeamComposition">
-           <div className="agileTeamComposition__title">
-               <Title data={t(data.title)}/>
-           </div>
-           <div className="agileTeamComposition__description">
-                <p>{t(data.description)}</p>
-           </div>
-           <br/><hr width="50%" size="5px" color="#78acfd"/>
-           <div className="agileTeamComposition__items">
-            {data.items.map((item, index) => {
-            return <div className ="items-container">
-                        <AgileTeamCompositionList data={item} key={index}/>
-            <div className ="items-content"><p>{t(item.subtitle)}</p></div>
-                    </div>
-            })}
-           </div>
-        </section>
-    );
-}
+const AgileTeamComposition = ({ data }) => {
+  const { t } = useTranslation();
+  return (
+    <section className="agileTeamComposition">
+      <div className="agileTeamComposition__title">
+        <Title data={t(data.title)} />
+      </div>
+      <div className="agileTeamComposition__description">
+        <p>{t(data.description)}</p>
+      </div>
+      <br />
+      <hr width="50%" size="5px" color="#78acfd" />
+      <div className="agileTeamComposition__items">
+        {data.items.map((item, index) => {
+          return (
+            <div className="items-container">
+              <AgileTeamCompositionList data={item} key={index} />
+              <div className="items-content">
+                <p>{t(item.subtitle)}</p>
+              </div>
+            </div>
+          );
+        })}
+      </div>
+    </section>
+  );
+};
 export default AgileTeamComposition;
--- src/components/sections/service/WebAppRequirement.js
+++ src/components/sections/service/WebAppRequirement.js
@@ -1,37 +1,29 @@
-import React from 'react';
-import Title from '../../common/Title'
-import Image from '../../common/Image'
-import WebAppReqItem from './WebAppReqItem'
-import './WebAppRequirement.css'
+import React from "react";
+import Title from "../../common/Title";
+import Image from "../../common/Image";
+import WebAppReqItem from "./WebAppReqItem";
+import "./WebAppRequirement.css";
 import { useTranslation } from "react-i18next";
 
-const WebAppRequirment = ({data}) => {
-    const { t } = useTranslation();
-    return(
-        <div className = "web-app-requirment">
-            <div className = "web-app-requirment__title">
-                <Title data = {t(data.title)}/>
-            </div>
-            <Image className = "web-app-requirment__image" {...data.image}/>
-            <div className = "web-app-requirment__content">
-                {
-                    data.items.map((item, index) => {
-                        return (
-                            <WebAppReqItem key = {index} data = {item} />
-                        )
-                    })
-                }
-            </div>
-            <div className = "web-app-requirment__comment">
-                {
-                    data.comment.map((item,index) => {
-                        return(
-                            <p key = {index}>{t(item)}</p>
-                        )
-                    })
-                }
-            </div>
-        </div>
-    )
-}
+const WebAppRequirment = ({ data }) => {
+  const { t } = useTranslation();
+  return (
+    <section className="web-app-requirment">
+      <div className="web-app-requirment__title">
+        <Title data={t(data.title)} />
+      </div>
+      <Image className="web-app-requirment__image" {...data.image} />
+      <div className="web-app-requirment__content">
+        {data.items.map((item, index) => {
+          return <WebAppReqItem key={index} data={item} />;
+        })}
+      </div>
+      <div className="web-app-requirment__comment">
+        {data.comment.map((item, index) => {
+          return <p key={index}>{t(item)}</p>;
+        })}
+      </div>
+    </section>
+  );
+};
 export default WebAppRequirment;
